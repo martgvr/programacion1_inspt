345a357fa8755cc39cdce8d0c41576758725a98e
Martin Guevara <martin_91@live.com.ar>
2023-06-30 08:44:20 -0300

    header exclude fix

---
+++ programacion-1/7 - 28-06/utilities/fns.h
@@ -0,0 +1,26 @@
+#include <time.h>
+
+void askData(int * var, char * string) {
+    int input;
+
+    do {
+        printf("Ingrese %s: ", string);
+        scanf("%d", &input);
+    } while (input < 0);
+
+    *var = input;
+}
+
+void printArray(int array[], int length) {
+    for (int i = 0; i < length; i++) {
+        printf("[%d] = %d\n", i, array[i]);
+    }
+}
+
+void fillRandArray(int array[], int length, int MAX_RAND, int MIN_RAND) {
+    srand(time(NULL));
+    
+    for (int i = 0; i < length; i++) {
+        array[i] = (rand() % (MAX_RAND - MIN_RAND)) + MIN_RAND;
+    }
+}
